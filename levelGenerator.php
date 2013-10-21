17e45a668b8a9e05bad6bdcf8bde93220d481613
Le Sanglier des Ardennes <lesanglierdesardennes@gmail.com>
2013-10-22 00:03:43 +0200

    test

--- levelGenerator.php
+++ levelGenerator.php
@@ -13,7 +13,6 @@ $county = 0;
 $mapId = 0;
 $mapCounter = 0;
 
-
 // World array initialization 
 $world = array(
             array (
@@ -24,28 +23,43 @@ $world = array(
 
 // Fill the world 
 foreach ($lines as $line) {
-    $posy = $county - (($county % 15) * 15);
+    $posy =  (($county - ($county % 15)) / 15) ;
 
-
+    //   Number total of line  Number of line by map   Number of map
+    //echo $county . " " .       $county % 15 . " " .    $posy . "\n";
     for($countx = 0; $countx < strlen($line); $countx++) {
-
+        $posx = ($countx - ($countx % 30)) / 30;
+        
         $tile = substr($line, $countx, 1);
-        $mapId = ($countx % 30) * ($county % 15);
 
-        $posx = $countx - (($countx % 30) * 30);
+        $mapId = ($posy * 15) + $posx;
+    
+        //echo  $county . " = (" .  $posx . ", " . $posy. ", " . $mapId . ") \n";
 
-        if ($tile == 0 ) {
-            $world[$mapId][$posy][$posx] = 0;
+        $tiley = $county - (($county % 15) * 15);
+        $tilex = $countx - (($countx % 30) * 30);
+
+        if ($tile == 0) {
+            $world[$mapId][$tiley][$tilex] = 0;
         }
-        if ($tile == 1 ) {
-            $world[$mapId][$posy][$posx] = 1;
+ 
+        if ($tile == 1) {
+            $world[$mapId][$tiley][$tilex] = 1;
         }
    
     }
+    //echo "*************************\n";
     $county++;
 }
 
+// Display world
+/*
+for($y = 0; $y < 15; $y++) {
+    for($x = 0; $x < 30; $x++) {
+        echo $world[1][$y][$x];
+    }
+    echo "\n";
+}*/
 
-// Display the world
-
+var_dump($world[2]);
 ?>
